6632f0055b80ac6c96ee2bff3fefbf1f178d11e8
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-11-21 12:41:28 +0300

    add test for 0 input

--- factorial/test/test_factorial.py
+++ factorial/test/test_factorial.py
@@ -8,7 +8,21 @@ class Testfibonacci(unittest.TestCase):
     Class contains test on the factorial function
     """
 
-    def test_fact_method_returns_correct_values(self):
+    def test_factorial_method_returns_correct_values(self):
+
+        """
+        Method tests whether factorial function returns correct values
+        """
 
         result = factorial.factorial(3)
         self.assertEqual(6, result)
+
+
+    def test_factorial_method_returns_1_for_0_input(self):
+
+        """
+        Method chacks whether factorial function returns 0 for input 1
+        """
+
+        result = factorial.factorial(0)
+        self.assertEqual(1, result)
